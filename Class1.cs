2058c98c0aa19f0b247abb407a3cc130d377f7d5
Molnar Laszlo <laszlo.molnar25@gmail.com>
2020-08-28 21:30:17 +0200

    Initial commit

---
+++ Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageBoxCloser
+{
+    public class Class1
+    {
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(int hWnd, uint Msg, int wParam, int lParam);
+        public const int WM_SYSCOMMAND = 0x0112;
+        public const int SC_CLOSE = 0xF060;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        internal static extern int MessageBox(IntPtr hWnd, string lpText, string lpCption, uint uType);
+
+        public static void CloseWindow(IntPtr window)
+        {
+            if (window != IntPtr.Zero)
+            {
+                Console.WriteLine("Window found, closing...");
+
+                SendMessage((int)window, WM_SYSCOMMAND, SC_CLOSE, 0);
+            }
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MessageBoxCloser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var window = Class1.FindWindow(null, "Ez a gép");
+            while (window != IntPtr.Zero)
+            {
+                Class1.CloseWindow(window);
+                window = Class1.FindWindow(null, "Ez a gép");
+
+            }
+            Console.WriteLine("Press a button to quit");
+            Class1.MessageBox(IntPtr.Zero, "Ablakok bezárva", "Done - Message", 0x00000000);
+        }
+    }
+}
